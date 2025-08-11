60a66e2d52e897ca9c440d49090450544c3a571f
Anushini <153724709+AnushiniAnanth@users.noreply.github.com>
2025-08-12 00:10:38 +0530

    Update MediTrackAppointmentApplication.java

--- src/main/java/meditrack/MediTrackAppointmentApplication.java
+++ src/main/java/meditrack/MediTrackAppointmentApplication.java
@@ -2,13 +2,12 @@ package meditrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.modelmapper.ModelMapper;
-@EnableFeignClients
-@EnableDiscoveryClient
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 @SpringBootApplication
+@CrossOrigin(origins = "*", maxAge=3600)
 public class MediTrackAppointmentApplication {
 
     public static void main(String[] args) {
