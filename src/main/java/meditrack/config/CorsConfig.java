e6d6792c432429bb87a5e9dfabe25f6d35cfb891
Munirathinam_E <144887857+Munirathinam-E@users.noreply.github.com>
2025-08-12 17:06:09 +0530

    Update CorsConfig.java

--- src/main/java/meditrack/config/CorsConfig.java
+++ src/main/java/meditrack/config/CorsConfig.java
@@ -24,7 +24,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/")
+        registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:3000",
                         "http://localhost:5173",
